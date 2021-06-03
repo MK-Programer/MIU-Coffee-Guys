c252b4829d3865f349ef36a9a7d352eba9c6bd70
Mostafa Khaled <Mostafa1810751@outlook.com>
2021-06-04 01:56:34 +0200

    Create Menu Bar.php

---
+++ Menu Bar.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="style.css">
+  <title>My Website</title>
+</head>
+<body>
+  <!-- Header -->
+  <section id="header">
+    <div class="header container">
+      <div class="nav-bar">
+        <div class="brand">
+          <a href="#hero"><h1><span>L</span> M <span> S</span></h1></a>
+        </div>
+        <div class="nav-list">
+          <div class="hamburger"><div class="bar"></div></div>
+          <ul>
+            <li><a href="#Home" data-after="Home">Home</a></li>
+            <!--<li><a href="#services" data-after="Service"></a></li>-->
+            <li><a href="#About Us" data-after="About">About Us</a></li>  
+            <li><a href="#Videos" date-after="Videos">Videos</a></li>
+            <li><a href="#Books" data-after="Books">Books</a></li>
+            <li><a href="#Contacts" data-after="Contact">Contacts</a></li>
+            <li><a href="#Logout">Logout</a></li>  
+          </ul>
+        </div>
+      </div>
+    </div>
+  </section>
+</body>
+</html>
